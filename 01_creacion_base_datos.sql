5e6d5b50d220d5b861e47f82449e8add2826efe1
sotom28 <rod.sotod@duocuc.cl>
2026-06-05 21:56:29 -0400

    cambios a data aws4

--- 01_creacion_base_datos.sql
+++ 01_creacion_base_datos.sql
@@ -8,7 +8,7 @@
 
 -- Eliminar base de datos si existe (para desarrollo)
 -- ADVERTENCIA: Esto eliminará todos los datos existentes
-
+-- 
 -- DROP DATABASE IF EXISTS proyecto_db;
 -- Crear base de datos principal
 CREATE DATABASE IF NOT EXISTS proyecto_db 
